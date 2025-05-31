175cfdb5149af58d51fe5173a47533db9f2b9c9c
yeojin12 <kyj713102@naver.com>
2025-05-31 13:29:21 +0900

    찐복

--- class/pages/partner.js
+++ class/pages/partner.js
@@ -81,7 +81,8 @@ export default function Partner() {
 
                   <button
                     className={cardStyles.searchButton}
-                    onClick={() => router.push(/map?keyword=${encodeURIComponent(p.name)})}
+                    onClick={() => router.push(`/map?keyword=${encodeURIComponent(p.name)}`)}
+
                   >
                     지도에서 보기
                   </button>
@@ -89,8 +90,8 @@ export default function Partner() {
 
                 <div className={cardStyles.cardRight}>
                   <img
-                    src={/data/partner/images/${p.name.replace(/\s/g, "_")}.jpg}
-                    alt={${p.name} 썸네일}
+                    src={'/data/partner/images/${p.name.replace(/\s/g, "_")}.jpg'}
+                    alt={'${p.name} 썸네일'}
                     onError={(e) => {
                       e.target.onerror = null;
                       e.target.src = "/data/image.jpg";
